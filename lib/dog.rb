4b35accda30efe5f0a4b818337c1a1d90c0199a9
Peter Bedrosian <peter_bedrosian@yahoo.com>
2020-02-24 18:44:06 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,16 @@
-# Your code goes here!
+class Dog
+  def bark
+    puts "woof!"
+  end
+
+  def name=(dog_name)
+      @this_dogs_name = dog_name
+    end
+
+    def name
+      @this_dogs_name
+    end
+  end
+
+  fido = Dog.new
+  fido.name = "Fido"
